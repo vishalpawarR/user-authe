684d4d8bb894d475e12b9e0e7d75b365205b5aeb
Vishal Pawar <vishalpawar425@gmail.com>
2023-01-05 17:21:55 +0530

    Checking error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import SignUp from './components/SignUp';
+import React from 'react';
+import { AuthProvider } from 'firebase/auth';
 
 function App() {
-  return "hello";
+  return (
+    <AuthProvider>
+      <Container className='d-flex align-items-center justify-content-center' style={{minHeight: '100vh'}}>
+        <div className="w-100" style={{maxWidth: '400px'}}>
+        <SignUp/>
+        </div>
+      </Container>
+    </AuthProvider>
+  );
 }
 
 export default App;
